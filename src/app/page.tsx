13aa6bd4c246b74fb3fcfeddd2811a0610e9bdd2
Sterphane Roque <stermiss2@gmail.com>
2024-07-29 14:49:35 -0300

    feat: navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,41 @@ import { truncateText } from '@/utils/truncate-text';
 import { useState } from 'react';
 import { GrLocation } from 'react-icons/gr';
 import { PiSuitcaseSimple } from 'react-icons/pi';
+import { NavSections } from '@/components/NavSections';
 
 export default function Home() {
    const [expanded, setExpanded] = useState(false);
+   const [menuItems, setMenuItems] = useState([
+      {
+         component: NavSections.Projects,
+         title: 'Projetos',
+         icon: <MdOutlineMailOutline className='text-2xl' />,
+         isActive: true,
+      },
+      {
+         component: NavSections.Formation,
+         title: 'Formação',
+         icon: <MdOutlineMailOutline className='text-2xl' />,
+         isActive: false,
+      },
+      {
+         component: NavSections.Experience,
+         title: 'Experiência',
+         icon: <MdOutlineMailOutline className='text-2xl' />,
+         isActive: false,
+      },
+      {
+         component: NavSections.Contact,
+         title: 'Contato',
+         icon: <MdOutlineMailOutline className='text-2xl' />,
+         isActive: false,
+      },
+   ]);
+
+   const currentContent = menuItems.find((item) => item.isActive);
+   const CurrentActiveSection =
+      currentContent?.component ?? NavSections.Projects;
+
    const text =
       'Sou Sterphane Roque, desenvolvedora front-end com mais de 2 anos de experiência em projetos freelance. Meu interesse em TI começou em 2019, durante meu curso técnico em informática, onde estudei C, C++, Java, PHP, SQL, HTML, CSS e JavaScript. Formei-me em 2021 e desenvolvi um projeto de conclusão de curso utilizando Java no Android Studio. Atualmente, me especializo em front-end e mobile com React e React Native, e pretendo expandir meus conhecimentos para o backend.';
    const limit = 340;
@@ -18,8 +50,24 @@ export default function Home() {
    };
    const textToShow = expanded ? text : truncateText(text, limit);
 
+   function handleNavigateToSection(index: number) {
+      setMenuItems((prev) => {
+         const updatedMenuItems = [...prev];
+
+         updatedMenuItems.forEach((menuItem) => {
+            if (menuItem.isActive) {
+               menuItem.isActive = false;
+            }
+         });
+
+         updatedMenuItems[index].isActive = true;
+
+         return updatedMenuItems;
+      });
+   }
+
    return (
-      <main className='flex flex-col p-4'>
+      <main className='flex flex-col'>
          <div className='flex flex-col text-center w-full items-center'>
             <Image
                src='https://github.com/Sterzinha.png'
@@ -30,31 +78,17 @@ export default function Home() {
             />
             <h3 className='text-sm text-[#50FA7B]'>Desenvolvedora Front-End</h3>
             <h1 className='font-bold text-2xl'>Sterphane Roque</h1>
-
-            {/* <a
-               href='mailto:stermiss2@gmail.com'
-               className='text-sm border-[1px] border-gray-50 w-fit flex items-center p-3 py-1 rounded-lg mb-3 gap-2'>
-               <MdOutlineMailOutline />
-               stermiss2@gmail.com
-            </a>
-            <a
-               href='https://api.whatsapp.com/send/?phone=559784437986&text&type=phone_number&app_absent=0'
-               target='_blank'
-               className='text-sm border-[1px] border-gray-50 w-fit flex items-center p-3 py-1 rounded-lg gap-2'>
-               <FaWhatsapp />
-               +55 97 98443-7986
-            </a> */}
          </div>
 
-         <div className='mt-10 flex flex-col gap-4 bg-[#343644] rounded-xl p-7'>
-            <h3 className='font-semibold text-lg text-[#BD93F9]'>Sobre mim</h3>
+         <div className='mt-10 mx-4 flex flex-col gap-4 bg-gray rounded-xl p-7'>
+            <h3 className='font-semibold text-lg text-primary'>Sobre mim</h3>
             <p className='text-lg text-gray-300 tracking-tighter text-justify border-t-[1px] border-b-[1px] py-3 border-gray-400'>
                {textToShow}
 
                {text.length > limit && (
                   <button
                      onClick={toggleText}
-                     className='text-[#BD93F9]'>
+                     className='text-primary'>
                      {expanded ? 'Ler menos' : 'Ler mais'}
                   </button>
                )}
@@ -70,6 +104,25 @@ export default function Home() {
                Desenvolvedora Front-End
             </span>
          </div>
+
+         <div>
+            <nav className='flex gap-3 overflow-x-scroll md:overflow-hidden'>
+               {menuItems.map((item, index) => (
+                  <a
+                     key={item.title}
+                     className={`cursor-pointer flex gap-1 items-center text-sm active:text-primary uppercase ${
+                        item.isActive
+                           ? 'border-b-2 border-primary text-primary py-2'
+                           : 'none'
+                     }`}
+                     onClick={() => handleNavigateToSection(index)}>
+                     {item.icon}
+                     {item.title}
+                  </a>
+               ))}
+            </nav>
+         </div>
+         <CurrentActiveSection />
       </main>
    );
 }
