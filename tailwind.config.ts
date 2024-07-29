7bc17e4022c8ccec4ac938cdb2a20fd7f8321d40
Sterphane Roque <stermiss2@gmail.com>
2024-07-29 14:47:16 -0300

    chore: theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            'gradient-conic':
+               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+         },
+         colors: {
+            primary: '#BD93F9',
+            secondary: '#50FA7B',
+            gray: '#343644',
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
 export default config;
