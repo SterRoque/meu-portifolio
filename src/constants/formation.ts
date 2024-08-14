442804a657dadde692f0c1bfe65b8bb1bfcc1af5
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2024-08-14 01:56:05 -0300

    fix: formation date

--- src/constants/formation.ts
+++ src/constants/formation.ts
@@ -1,7 +1,7 @@
 export const formations = [
    {
       name: 'Instituto Federal do Amazonas (IFAM)',
-      formationDate: '2015 - 2021',
+      formationDate: '2019 - 2021',
       course: 'Técnico em Informática',
       description:
          'Comecei a fazer este curso em 2019, e como eu sempre gostei de informática, não tive muita dificuldade. Foi no mesmo que eu conheci o mundo da programação, fazendo eu me apaixonar no momento que estudei lógica de programação.',
