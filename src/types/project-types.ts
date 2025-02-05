f0471495ab611b738a8f3c47be375e8734bf0121
Sterphane Roque <stermiss2@gmail.com>
2025-02-05 16:52:39 -0300

    feat:add types

--- src/types/project-types.ts
+++ src/types/project-types.ts
@@ -1,7 +1,13 @@
 export interface IProject {
+   id: string;
    img: string;
+   imgScreens?: string[];
    title: string;
    description: string;
+   descriptionDetails: string;
+   technologies: string[];
+   features: string[];
+   conclusion: string;
    urlSite?: string;
    urlGithub?: string;
    stacks: string[];
