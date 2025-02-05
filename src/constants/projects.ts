495cdc41e182e4690e2105f1286cb24a04d0b7e0
Sterphane Roque <stermiss2@gmail.com>
2025-02-05 16:53:54 -0300

    feat: add imgScreens, descriptionDetails, technologies, features, conclusion in projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,27 +1,83 @@
 export const projects = [
    {
+      id: '1',
       title: 'Lista Fácil',
       img: '/imgs/lista-facil.png',
+      imgScreens: [
+         '/imgs/telas-lista-facil/tela-1.png',
+         '/imgs/telas-lista-facil/tela-2.png',
+         '/imgs/telas-lista-facil/tela-3.png',
+         '/imgs/telas-lista-facil/tela-4.png',
+         '/imgs/telas-lista-facil/tela-5.png',
+         '/imgs/telas-lista-facil/tela-6.png',
+         '/imgs/telas-lista-facil/tela-7.png',
+         '/imgs/telas-lista-facil/tela-8.png',
+      ],
       description:
          'O Lista Fácil é um aplicativo intuitivo que transforma suas compras no supermercado em uma experiência organizada e eficiente. Adicione produtos, insira preços e quantidades, e o app calcula automaticamente o total da compra. Ideal para quem quer manter o orçamento sob controle com praticidade.',
       urlSite: '',
       urlGithub: 'https://github.com/SterRoque/lista-facil-app',
-      stacks: [
-         'React Native',
-         'Styled Components',
-         'TypeScript',
-         'Figma',
-         'SQLite',
+      descriptionDetails:
+         'O Lista Fácil App é um projeto desenvolvido para oferecer uma solução eficiente e intuitiva para a criação e gerenciamento de listas de compras. Com uma interface moderna e funcionalidades que facilitam o planejamento financeiro, o aplicativo ajuda os usuários a economizar tempo e dinheiro de forma prática.',
+      technologies: [
+         '<strong>Front-end:</strong> React Native, StyleSheet, TypeScript',
+         '<strong>Banco de Dados:</strong> SQLite',
       ],
+      features: [
+         '<strong>Criação de Listas Personalizadas:</strong> Adicione produtos com nome, quantidade e preço unitário.',
+         '<strong>Edição Simples:</strong> Modifique ou remova produtos com facilidade.',
+         '<strong>Cálculo Automático:</strong> Soma automaticamente os valores dos itens para um melhor controle de gastos.',
+      ],
+
+      conclusion:
+         'O Lista Fácil App é um projeto inovador que simplifica a experiência de compras, tornando-a mais organizada e econômica. Com tecnologia de ponta e um design intuitivo, ele se destaca como uma solução eficaz para qualquer consumidor.',
+
+      stacks: ['React Native', 'StyleSheet', 'TypeScript', 'Figma', 'SQLite'],
    },
    {
+      id: '2',
       title: 'RedePet App',
       img: '/imgs/redepet-app.png',
+      imgScreens: [
+         '/imgs/telas-redepet-app/tela-1.png',
+         '/imgs/telas-redepet-app/tela-2.png',
+         '/imgs/telas-redepet-app/tela-3.png',
+         '/imgs/telas-redepet-app/tela-4.png',
+         '/imgs/telas-redepet-app/tela-5.png',
+         '/imgs/telas-redepet-app/tela-6.png',
+         '/imgs/telas-redepet-app/tela-7.png',
+         '/imgs/telas-redepet-app/tela-8.png',
+         '/imgs/telas-redepet-app/tela-9.png',
+         '/imgs/telas-redepet-app/tela-10.png',
+         '/imgs/telas-redepet-app/tela-11.png',
+         '/imgs/telas-redepet-app/tela-12.png',
+         '/imgs/telas-redepet-app/tela-13.png',
+         '/imgs/telas-redepet-app/tela-14.png',
+         '/imgs/telas-redepet-app/tela-15.png',
+         '/imgs/telas-redepet-app/tela-16.png',
+         '/imgs/telas-redepet-app/tela-17.png',
+      ],
+
       description:
-         'RedePet é o aplicativo ideal para garantir a segurança dos seus pets. Com ele, você pode cadastrar seus animais e rastreá-los em tempo real usando o QR code na coleira. Sempre que o QR code for escaneado, você receberá uma notificação com a localização exata do seu pet, proporcionando tranquilidade e proteção para seu amigo de quatro patas. \n\n Lançamento em breve!!',
+         'RedePet é o aplicativo ideal para garantir a segurança dos seus pets. Com ele, você pode cadastrar seus animais e rastreá-los em tempo real usando o QR code na coleira. Sempre que o QR code for escaneado, você receberá uma notificação com a localização exata do seu pet, proporcionando tranquilidade e proteção para seu amigo de quatro patas.',
+      descriptionDetails:
+         'O RedePet App é uma plataforma inovadora desenvolvida para ajudar tutores a manterem seus animais de estimação seguros. Através de um sistema de rastreamento por QR code, o aplicativo permite que qualquer pessoa que encontre um pet perdido possa escanear o código e entrar em contato com o dono, garantindo uma localização rápida e eficiente.',
       urlSite:
          'https://play.google.com/store/apps/details?id=app.redepet.localiza',
       urlGithub: '',
+      technologies: [
+         '<strong>Front-end:</strong> React Native, Styled Components, TypeScript',
+         '<strong>Back-end:</strong> NodeJS, NestJS, Golang, Firebase',
+         '<strong>Banco de Dados:</strong> PostgreSQL',
+      ],
+      features: [
+         '<strong>Cadastro de Pets:</strong> Usuários podem registrar seus animais com informações como nome, idade, raça e fotos.',
+         '<strong>Geração e Cadastro de QR Code: </strong> Cada pet recebe um QR code exclusivo para ser anexado à coleira, permitindo fácil identificação e recuperação em caso de perda.',
+         '<strong>Rastreamento do pet:</strong> Quando um pet perdido é encontrado, o QR code pode ser escaneado e o dono recebe uma notificação com a localização exata.',
+      ],
+
+      conclusion:
+         'O RedePet App é uma solução inovadora que une tecnologia e amor pelos animais, proporcionando mais segurança e tranquilidade para tutores. Com funcionalidades avançadas e uma interface intuitiva, ele se destaca como uma ferramenta essencial para qualquer dono de pet que deseja garantir o bem-estar do seu animal.',
       stacks: [
          'React Native',
          'Styled Components',
@@ -35,29 +91,90 @@ export const projects = [
       ],
    },
    {
+      id: '3',
       title: 'RedePet Web',
       img: '/imgs/redepet-web.png',
+      imgScreens: ['/imgs/telas-redepet-web/tela-desktop.png'],
       description: 'Essa é a uma página de download do aplicativo RedePet.',
+      descriptionDetails:
+         'Essa é uma página de download do aplicativo RedePet que foi desenvolvida para oferecer aos usuários uma experiência intuitiva e eficiente para baixar o aplicativo.',
+
       urlSite: 'https://redepet.app/',
       urlGithub: '',
+
+      technologies: [
+         '<strong>Front-end:</strong> AstroJS, Tailwind, TypeScript',
+      ],
+      features: [
+         'Exibição clara das opções de download para Android e iOS (atualmento somente Android)',
+         'Botão de redirecionamento direto para as lojas de aplicativos',
+         'Informativo sobre os principais recursos do aplicativo',
+      ],
+
+      conclusion:
+         'A página de download do RedePet é um ponto fundamental na jornada do usuário, garantindo que ele encontre rapidamente onde baixar o aplicativo e compreenda seus benefícios. Com uma interface moderna, responsiva e objetiva, a página facilita o acesso ao app e contribui para a segurança dos pets. O RedePet continua evoluindo para oferecer cada vez mais funcionalidades e melhorar a experiência de seus usuários.',
       stacks: ['AstroJS', 'Tailwind', 'TypeScript', 'Figma'],
    },
    {
+      id: '4',
       title: 'Amigo Peludo',
       img: '/imgs/amigo-peludo.png',
+      imgScreens: ['/imgs/telas-amigo-peludo/tela-desktop.png'],
+
       description:
          'Landing Page estatica de adoção de pets criando em angular apenas para fins de aprendizado',
+      descriptionDetails:
+         'A Amigo Peludo é uma landing page estática desenvolvida em Angular com o objetivo de simular uma plataforma de adoção de pets. Este projeto foi criado exclusivamente para fins de aprendizado, proporcionando uma experiência prática no desenvolvimento de interfaces estaticas.',
+
       urlSite: 'https://amigopeludo.netlify.app/',
       urlGithub: 'https://github.com/SterRoque/landing-page-pet',
+      technologies: [
+         '<strong>Front-end:</strong> Angular, Tailwind, TypeScript',
+      ],
+
+      features: [
+         '<strong>Apresentação de Pets Disponíveis:</strong> Cards exibindo informações dos animais para adoção.',
+         '<strong>Seção Informativa:</strong> Explica como funciona a adoção.',
+      ],
+
+      conclusion:
+         'A landing page Amigo Peludo serviu como um excelente exercício para aprimorar o conhecimento em Angular. Embora seja um projeto simples, ele representa um passo importante no aprendizado da tecnologia e no desenvolvimento de interfaces interativas e intuitivas.',
       stacks: ['Angular', 'TypeScript', 'Tailwind'],
    },
    {
+      id: '5',
       title: 'Sintese Proteica App',
       img: '/imgs/sintese.jpeg',
+      imgScreens: [
+         '/imgs/telas-sintese-proteica/tela-1.png',
+         '/imgs/telas-sintese-proteica/tela-2.png',
+         '/imgs/telas-sintese-proteica/tela-3.png',
+         '/imgs/telas-sintese-proteica/tela-4.png',
+         '/imgs/telas-sintese-proteica/tela-5.png',
+         '/imgs/telas-sintese-proteica/tela-6.png',
+         '/imgs/telas-sintese-proteica/tela-7.png',
+         '/imgs/telas-sintese-proteica/tela-8.png',
+         '/imgs/telas-sintese-proteica/tela-9.png',
+      ],
+
       description:
-         'Aplicativo Educacional voltado para demostrar processo de duplicação, transcrição e tradução do DNA. Será atualizado em breve com novas stacks.',
+         'Aplicativo Educacional voltado para demostrar processo de duplicação, transcrição e tradução do DNA.',
+
+      descriptionDetails:
+         'O aplicativo Síntese Proteica é uma ferramenta educacional interativa que visa demonstrar os processos de duplicação, transcrição e tradução do DNA. Com uma interface visual e dinâmica, ele facilita o aprendizado de biologia molecular, tornando conceitos complexos mais acessíveis para estudantes e professores. O foco é proporcionar uma experiência educativa interativa e envolvente.',
       urlSite: '',
       urlGithub: 'https://github.com/SterRoque/app-sintese-de-proteica',
-      stacks: ['Java', 'Android Studio'],
+
+      technologies: ['<strong>Front-end:</strong> React Native, NativeWind'],
+
+      features: [
+         '<strong>Duplicação do DNA:</strong> O aplicativo demonstra a conversão das fitas de DNA durante o processo de duplicação. O usuário pode optar por dois modos: <b>auto duplicação</b> (onde o processo ocorre automaticamente) ou modo manual (onde o usuário realiza o processo, convertendo as fitas de DNA por conta própria).',
+         '<strong>Transcrição do DNA:</strong> Apresenta a conversão da fita de DNA em RNA mensageiro (mRNA). Assim como na duplicação, o usuário pode escolher entre <b>auto transcrição</b> (automática) ou <b>modo manual</b>, realizando a transcrição por conta própria.',
+         '<strong>Tradução do RNA:</strong> Demonstra o processo de tradução do mRNA em uma sequência de aminoácidos para a formação de proteínas. o modo automático, o processo ocorre de forma automática, e no modo manual, o usuário converte as sequências de RNA para aminoácidos, utilizando a tabela de códons como auxílio.',
+         '<strong>Histórico de Fitas de DNA:</strong> O aplicativo armazena um histórico das fitas de DNA trabalhadas, permitindo que o usuário acompanhe as modificações feitas durante os processos de duplicação, transcrição e tradução.',
+      ],
+      conclusion:
+         'O Síntese Proteica oferece uma maneira inovadora de aprender sobre os processos biológicos fundamentais, utilizando simulações interativas para uma compreensão mais clara e dinâmica. A ferramenta visa enriquecer a educação em biologia, tornando o aprendizado mais acessível e estimulante, com planos de expansão para novos recursos e funcionalidades.',
+      stacks: ['React Native', 'NativeWind'],
    },
 ];
